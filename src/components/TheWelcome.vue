03ba66e52c76722901bca4f5e8789817fc518cb7
Anggara212 <anggarafabiano5@gmail.com>
2022-08-23 16:51:26 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,43 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon/>
+    </template>
+    <template #heading>Nama</template>
+    <li>Anggara Fabiano Putra Sugama</li>
+  </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Role</template>
+    <li>Software Engineer</li>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Alamat</template>
+    <li>Kampung Areman Rt 08 rw 08,<br> cimanggis, depok, jawa barat</li>   
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact</template>
+    <li>08212-480-4356</li>
+    <li>0895-6168-888-040</li>
+    <li>Anggarafabiano4@gmail.com</li>
+  </WelcomeItem>
+  
+</template>
---
+++ src/views/SkillView.vue
@@ -0,0 +1,72 @@
+<!-- <script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script> -->
+<!-- <template>
+  <div class="skill">
+    <title>Skill</title>
+    <h1>This is an Skill page</h1>
+  </div>
+</template> -->
+
+<style>
+@media (min-width: 1024px) {
+  .img1{
+    padding-top: 27px;
+    margin-bottom: 27px;
+   
+  }
+  .img2{
+    padding-top: 27px;
+    margin-bottom: 27px;
+   
+  }
+  .img3{
+  padding-top: 27px;
+  margin-bottom: 27px;
+  
+  }
+
+  .img4{
+  padding-top: 27px;
+  margin-bottom: 27px;
+  
+  }
+}
+</style>
+
+<template>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <div class="container1">
+    <div class="img1">
+      <img src="src/assets/html5-logo-31813.png" width="150"  alt="">
+    </div>
+    <div class="w3-light-grey">
+  <div class="w3-orange" style="width:80%">80%</div>
+</div>
+ </div>
+ <div class="container2">
+    <div class="img2">
+      <img src="src/assets/css.png" width="170" alt="">
+    </div>
+    <div class="w3-light-grey">
+  <div class="w3-blue" style="width:75%">75%</div>
+</div>
+ </div>
+  <div class="container3">
+    <div class="img3">
+      <img src="src/assets/figma (2).png" width="170" alt="">
+    </div>
+    <div class="w3-light-yellow">
+  <div class="w3-yellow" style="width:70%">70%</div>
+</div>
+ </div>
+  <div class="container4">
+    <div class="img4">
+      <img src="src/assets/laravel.png" width="200" alt="">
+    </div>
+    <div class="w3-light-yellow">
+  <div class="w3-orange" style="width:65%">65%</div>
+</div>
+ </div>
+</template>
+
